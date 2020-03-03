61577852b01ec672c0bea996dfd4c56c62ada25c
Joshua Coffey <geel@scrap.tf>
2020-03-03 00:44:04 -0600

    Export compile by default

--- litfile-compiler/src/main.js
+++ litfile-compiler/src/main.js
@@ -1,7 +1,7 @@
 import LitParser from './lit_parser.js';
 import LitCompiler from './lit_compiler.js';
 
-export function compile(litfile) {
+export default function compile(litfile) {
     var parser = new LitParser();
     var compiler = new LitCompiler();
 
